7ceac66615a8f76fbd4738c31be89729306c5c05
mecaneer23 <mecaneer23@gmail.com>
2024-05-19 20:30:32 -0500

    feat: add relative time

--- get_commit.py
+++ get_commit.py
@@ -3,7 +3,56 @@
 Simple python implementation of Most Recent Git Commit for the terminal
 """
 
+from datetime import datetime, timezone
 import requests
+
+
+def get_relative_time(date: datetime):
+    """Take a datetime and return its "age" as a string.
+    The age can be in second, minute, hour, day, month or year. Only the
+    biggest unit is considered, e.g. if it's 2 days and 3 hours, "2 days" will
+    be returned.
+    Make sure date is not in the future, or else it won't work.
+    Original Gist by 'zhangsen' @ https://gist.github.com/zhangsen/1199964
+    Original Gist by 'jonlabelle' @
+    https://gist.github.com/jonlabelle/7d306575cbbd34b154f87b1853d532cc
+    """
+
+    class FormatDelta:  # pylint: disable=too-few-public-methods
+        """
+        Wrapper around format method to convert datetime to
+        relative time string
+        """
+
+        def __init__(self, dt: datetime) -> None:
+            now = datetime.now(timezone.utc)
+            delta = now - dt
+            self.day = delta.days
+            self.second = delta.seconds
+            self.year, self.day = divmod(self.day, 365)
+            self.month, self.day = divmod(self.day, 30)
+            self.hour, self.second = divmod(self.second, 3600)
+            self.minute, self.second = divmod(self.second, 60)
+
+        @staticmethod
+        def _formatn(n: int, s: str) -> str:
+            """Add "s" if it's plural"""
+
+            if n == 1:
+                return f"1 {s}"
+            if n > 1:
+                return f"{n} {s}s"
+            return "You can't have less than one unit"
+
+        def format(self):
+            """Public method to get the relative time"""
+            for period in ["year", "month", "day", "hour", "minute", "second"]:
+                n = getattr(self, period)
+                if n >= 1:
+                    return f"{self._formatn(n, period)} ago"
+            return "just now"
+
+    return FormatDelta(date).format()
 
 
 def get_most_recent_repo(user: str) -> str:
@@ -19,10 +68,11 @@ def get_most_recent_commit(user: str) -> dict[str, str]:
     """Get the most recent commit for a given user"""
     repo = get_most_recent_repo(user)
     data = requests.get(f"https://api.github.com/repos/{user}/{repo}/commits").json()[0]
+    date = data["commit"]["author"]["date"]
     return {
         "repo": repo,
         "message": data["commit"]["message"],
-        "date": data["commit"]["author"]["date"],
+        "date": f"{date} ({get_relative_time(datetime.fromisoformat(date))})",
         "url": data["html_url"],
         "sha": data["sha"],
     }
